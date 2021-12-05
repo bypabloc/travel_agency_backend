3af6a979be5a28990a36b0101ba73d8bfcd0499f
Your Name <pacg1991@gmail.com>
2021-12-05 11:31:38 -0500

    pagination fixed limit

--- project/api/helpers/model_apply_pagination.py
+++ project/api/helpers/model_apply_pagination.py
@@ -5,17 +5,18 @@ def model_apply_pagination(query, params):
     limit = params['limit']
     offset = int(params['offset'])
 
-    list = query[offset:limit]
+    list = query[offset:offset+limit]
 
     records_filtered = list.count()
 
     return {
-        'page': offset / limit + 1,
-        'current_page': offset,
+        'page': math.ceil(offset / limit + 1),
+        'offset' : offset,
+        'limit' : limit,
         'per_page' : limit,
+        'records_total' : records_filtered,
         'last_page': math.ceil(records_filtered / limit),
         'next_page': offset + 1 if (offset < records_filtered / limit) else None,
-        'prev_page': offset - 1 if (offset > 0) else None,
-        # 'prev_page': offset - 1 if (offset > 1) else None,
+        'prev_page': offset - 1 if (offset > 1) else None,
         'list': list.all()
     }
