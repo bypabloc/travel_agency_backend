5d96efc1e62a3f846e5fcef40b73fd76471e9b20
Your Name <pacg1991@gmail.com>
2021-12-06 12:31:05 -0500

    Update JourneyRepository.py

--- project/api/repository/JourneyRepository.py
+++ project/api/repository/JourneyRepository.py
@@ -31,6 +31,7 @@ class JourneyListForm():
             "id",
             "location_origin_data",
             "location_destination_data",
+            "duration_in_seconds",
             "is_active",
             "created_at",
             "updated_at",
